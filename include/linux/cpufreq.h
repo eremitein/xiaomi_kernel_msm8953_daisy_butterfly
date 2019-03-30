b7ef60209d41b7c574449791a0b7623e2a9db934
Victor Bo <bvoid@yandex.ru>
2019-03-30 23:31:34 +0000

    cpufreq: add electron
     	modified:   drivers/cpufreq/Kconfig
     	modified:   drivers/cpufreq/Makefile
     	new file:   drivers/cpufreq/cpufreq_electron.c
     	modified:   include/linux/cpufreq.h
     	new file:   include/trace/events/cpufreq_electron.h

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -531,6 +531,9 @@ extern struct cpufreq_governor cpufreq_gov_zzmoove;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_IMPULSE)
 extern struct cpufreq_governor cpufreq_gov_impulse;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_impulse)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ELECTRON)
+extern struct cpufreq_governor cpufreq_gov_electron;
+#define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_electron)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SCHED)
 extern struct cpufreq_governor cpufreq_gov_sched;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_sched)
