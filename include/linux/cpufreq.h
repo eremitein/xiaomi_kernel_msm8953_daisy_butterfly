9a1226ca5d66834104c6c9390363366343bf54a6
Victor Bo <bvoid@yandex.ru>
2019-03-30 22:32:43 +0000

    cpufreq: add lionheart
     	modified:   arch/arm64/configs/daisy-zero_defconfig
     	modified:   drivers/cpufreq/Kconfig
     	modified:   drivers/cpufreq/Makefile
     	new file:   drivers/cpufreq/cpufreq_lionheart.c
     	modified:   include/linux/cpufreq.h

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -519,6 +519,9 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_RELAXED)
 extern struct cpufreq_governor cpufreq_gov_relaxed;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_relaxed)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LIONHEART)
+extern struct cpufreq_governor cpufreq_gov_lionheart;
+#define CPUFREQ_DEFAULT_GOVERNOR 	(&cpufreq_gov_lionheart)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SCHED)
 extern struct cpufreq_governor cpufreq_gov_sched;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_sched)
