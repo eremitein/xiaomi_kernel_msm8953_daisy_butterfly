01a4a3823ab0875a82ee2c8990f3f0243bdd0d46
Victor Bo <bvoid@yandex.ru>
2019-05-07 20:29:23 +0000

    cpufreq: add interactivex
     	modified:   arch/arm64/configs/daisy-zero_defconfig
     	modified:   drivers/cpufreq/Kconfig
     	modified:   drivers/cpufreq/Makefile
     	new file:   drivers/cpufreq/cpufreq_interactivex.c
     	modified:   include/linux/cpufreq.h

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -549,6 +549,9 @@ extern struct cpufreq_governor cpufreq_gov_clarity;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_CONSERVATIVEX)
 extern struct cpufreq_governor cpufreq_gov_conservativex;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_conservativex)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVEX)
+extern struct cpufreq_governor cpufreq_gov_interactivex;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interactivex)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SCHED)
 extern struct cpufreq_governor cpufreq_gov_sched;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_sched)
