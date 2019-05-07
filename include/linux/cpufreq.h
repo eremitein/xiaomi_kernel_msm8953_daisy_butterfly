0eed9073b926a44c44e6020dd99bdc5b7fb5963b
Victor Bo <bvoid@yandex.ru>
2019-05-07 21:08:41 +0000

    cpufreq: add ondemandx
     	modified:   arch/arm64/configs/daisy-zero_defconfig
     	modified:   drivers/cpufreq/Kconfig
     	modified:   drivers/cpufreq/Makefile
     	new file:   drivers/cpufreq/cpufreq_ondemandx.c
     	modified:   include/linux/cpufreq.h

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -552,6 +552,9 @@ extern struct cpufreq_governor cpufreq_gov_conservativex;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVEX)
 extern struct cpufreq_governor cpufreq_gov_interactivex;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interactivex)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ONDEMANDX)
+extern struct cpufreq_governor cpufreq_gov_ondemandx;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_ondemandx)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SCHED)
 extern struct cpufreq_governor cpufreq_gov_sched;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_sched)
