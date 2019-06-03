f600e3d91091ac445602de6a91160f91e8ca01a1
Victor Bo <bvoid@yandex.ru>
2019-06-03 05:19:05 +0000

    cpufreq: add xperience
     	modified:   arch/arm64/configs/daisy-zero_defconfig
     	modified:   drivers/cpufreq/Kconfig
     	modified:   drivers/cpufreq/Makefile
     	new file:   drivers/cpufreq/cpufreq_xperience.c
     	modified:   include/linux/cpufreq.h

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -582,6 +582,9 @@ extern struct cpufreq_governor cpufreq_gov_despair;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BARRY_ALLEN)
 extern struct cpufreq_governor cpufreq_gov_barry_allen;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_barry_allen)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_XPERIENCE)
+extern struct cpufreq_governor cpufreq_gov_xperience;
+#define CPUFREQ_DEFAULT_GOVERNOR 	(&cpufreq_gov_xperience)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SCHED)
 extern struct cpufreq_governor cpufreq_gov_sched;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_sched)
