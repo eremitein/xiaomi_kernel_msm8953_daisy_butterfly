18de77f9160610e03a0b7a57b3159e246c339cd9
Victor Bo <bvoid@yandex.ru>
2019-06-23 02:07:17 +0500

    cpufreq: add chill
    	modified:   arch/arm64/configs/daisy-zero_defconfig
    	modified:   drivers/cpufreq/Kconfig
    	modified:   drivers/cpufreq/Makefile
    	new file:   drivers/cpufreq/cpufreq_chill.c
    	modified:   include/linux/cpufreq.h

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -588,6 +588,9 @@ extern struct cpufreq_governor cpufreq_gov_xperience;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ALESSA)
 extern struct cpufreq_governor cpufreq_gov_alessa;
 #define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_alessa)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_CHILL)
+extern struct cpufreq_governor cpufreq_gov_chill;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_chill)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SCHED)
 extern struct cpufreq_governor cpufreq_gov_sched;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_sched)
