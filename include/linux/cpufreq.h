4a6e666b55a7b8ddabed06b237b1ebb1620c9abf
Victor Bo <bvoid@yandex.ru>
2019-06-23 05:09:09 +0500

    cpufreq: add ironactive
    	modified:   arch/arm64/configs/daisy-zero_defconfig
    	modified:   drivers/cpufreq/Kconfig
    	modified:   drivers/cpufreq/Makefile
    	new file:   drivers/cpufreq/cpufreq_ironactive.c
    	modified:   include/linux/cpufreq.h
    	new file:   include/trace/events/cpufreq_ironactive.h

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -594,6 +594,9 @@ extern struct cpufreq_governor cpufreq_gov_chill;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ONDEMANDPLUS)
 extern struct cpufreq_governor cpufreq_gov_ondemandplus;
 #define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_ondemandplus)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_IRONACTIVE)
+extern struct cpufreq_governor cpufreq_gov_ironactive;
+#define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_ironactive)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SCHED)
 extern struct cpufreq_governor cpufreq_gov_sched;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_sched)
