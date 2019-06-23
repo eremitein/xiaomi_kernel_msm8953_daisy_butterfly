e75ffd75fc2015246dbbb5aa08306d2ca48dbe65
Victor Bo <bvoid@yandex.ru>
2019-06-23 05:23:49 +0500

    cpufreq: add cultivation
    	modified:   arch/arm64/configs/daisy-zero_defconfig
    	modified:   drivers/cpufreq/Kconfig
    	modified:   drivers/cpufreq/Makefile
    	new file:   drivers/cpufreq/cpufreq_cultivation.c
    	modified:   include/linux/cpufreq.h

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -597,6 +597,9 @@ extern struct cpufreq_governor cpufreq_gov_ondemandplus;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_IRONACTIVE)
 extern struct cpufreq_governor cpufreq_gov_ironactive;
 #define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_ironactive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_CULTIVATION)
+extern struct cpufreq_governor cpufreq_gov_cultivation;
+#define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_cultivation)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SCHED)
 extern struct cpufreq_governor cpufreq_gov_sched;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_sched)
